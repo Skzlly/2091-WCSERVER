8da689093175bd762eed56505f3e6f93b27821b2
Skzlly <123310695+Skzlly@users.noreply.github.com>
2023-01-30 13:09:41 +0800

    1 file modified

--- Prelims/Prelim-Lab Activity 3/Lab-Act-3-NodeJS-Server.js
+++ Prelims/Prelim-Lab Activity 3/Lab-Act-3-NodeJS-Server.js
@@ -6,19 +6,19 @@ var http = require('http');
 
 var server = http.createServer(function (req, res) {
   if (req.url == '/') {
-    res.writeHead(200, { 'Content-Type': 'text.html' });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
       '<html><h1>Welcome to my Node.js Application</h1><body><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>'
     );
     res.end();
   } else if (req.url == '/student') {
-    res.writeHead(200, { 'Content-Type': 'text.html' });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
       '<html><h1>Welcome to my Node.js Application</h1><body><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>'
     );
     res.end();
   } else if (req.url == '/admin') {
-    res.writeHead(200, { 'Content-Type': 'text.html' });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body><p>This is admin Page.</p></body></html>');
     res.end();
   } else res.end('Invalid Request!');
