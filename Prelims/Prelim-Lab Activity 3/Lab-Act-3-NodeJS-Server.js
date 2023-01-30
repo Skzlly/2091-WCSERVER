fd876d0488820f901a67d76ad1419edf6e0e48f8
Skzlly <123310695+Skzlly@users.noreply.github.com>
2023-01-30 13:02:09 +0800

    1 file modified

--- Prelims/Prelim-Lab Activity 3/Lab-Act-3-NodeJS-Server.js
+++ Prelims/Prelim-Lab Activity 3/Lab-Act-3-NodeJS-Server.js
@@ -1,31 +1,29 @@
-localStorage// Task Name: LAB 3
+// Task Name: LAB 3
 // Garcia, Louis Jae F.
-// WD 201 
+// WD 201
 
 var http = require('http');
 
-var server = http.createServer(function (req, res){
-  if (req.url == '/'){
-    res.writeHead(200, {'Content-Type': 'text.html'});
-    res.write('<html><h1>Welcome to my Node.js Application</h1><body><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>');
+var server = http.createServer(function (req, res) {
+  if (req.url == '/') {
+    res.writeHead(200, { 'Content-Type': 'text.html' });
+    res.write(
+      '<html><h1>Welcome to my Node.js Application</h1><body><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>'
+    );
     res.end();
-
-  }
-  else if (req.url == "/student"){
-    res.writeHead(200, {'Content-Type': 'text.html'});
-    res.write('<html><h1>Welcome to my Node.js Application</h1><body><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>');
+  } else if (req.url == '/student') {
+    res.writeHead(200, { 'Content-Type': 'text.html' });
+    res.write(
+      '<html><h1>Welcome to my Node.js Application</h1><body><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>'
+    );
     res.end();
-  }
-  else if (req.url == "/admin") {
-    res.writeHead(200, {'Content-Type': 'text.html'});
+  } else if (req.url == '/admin') {
+    res.writeHead(200, { 'Content-Type': 'text.html' });
     res.write('<html><body><p>This is admin Page.</p></body></html>');
     res.end();
-  }
-  else 
-    res.end('Invalid Request!');
-
+  } else res.end('Invalid Request!');
 });
 
 server.listen(5000);
 
-console.log('Node.js web server at port 5000 is running..')
+console.log('Node.js web server at port 5000 is running..');
