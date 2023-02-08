d0dbf58afb0e519ed3630407f34109087b3f61d7
Skzlly <123310695+Skzlly@users.noreply.github.com>
2023-02-08 15:59:23 +0800

    2 files added

---
+++ Prelims/Prelim-Exam-Hands-On/app.js
@@ -0,0 +1,6 @@
+var name = require('./greet.js');
+var name = 'Louis';
+
+name.greet(name);
+name.congratulations(name);
+name.Goodbye(name);
---
+++ Prelims/Prelim-Exam-Hands-On/prelim.js
@@ -0,0 +1,13 @@
+var prelim = {
+  greet: function (name) {
+    console.log('Hello, ' + name + '!');
+  },
+  congratulations: function (name) {
+    console.log('Congratulations, ' + name + '!');
+  },
+  Goodbye: function (name) {
+    console.log('Goodbye, ' + name + '!');
+  },
+};
+
+module.exports = prelim;
